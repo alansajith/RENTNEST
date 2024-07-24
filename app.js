a72a0b43866e23b2fe21237339791d1cc0fbb7b7
Your Name <you@example.com>
2024-07-24 19:38:35 +0300

    added use error middleware

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/RENTNEST");
 }
 
-app.use((req, res, next) => {
+app.use((err,req, res, next) => {
   res.send("Something went wrong");
 });
 
